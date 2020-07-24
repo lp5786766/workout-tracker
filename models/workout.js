6b7cd73469deabaeccc94203b790b164088dc3e0
Luba Pecheneva <lp5786766@gmail.com>
2020-07-24 12:01:55 -0400

    ran seed successfully

--- models/workout.js
+++ models/workout.js
@@ -2,18 +2,41 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const WorkoutSchema = new Schema({
-  day: new Date().setDate(new Date().getDate()-10),
+  day: {
+    type: Date,
+    default: Date.now,
+  },
   exercises: [
     {
-      type: "resistance",
-      name: "Bicep Curl",
-      duration: 20,
-      weight: 100,
-      reps: 10,
-      sets: 4
-    }
-  ]
-},);
+      type: {
+        type: String,
+        trim: true,
+        required: 'Enter the type of exercise',
+      },
+      name: {
+        type: String,
+        trim: true,
+        required: 'Enter the name of exercise',
+      },
+      duration: {
+        type: Number,
+        required: "Enter duration"
+      },
+      weight: {
+        type: Number
+      },
+      reps: {
+        type: Number
+      },
+      sets: {
+        type: Number
+      },
+      distance: {
+        type: Number
+      }
+    },
+  ],
+});
 
 const Workout = mongoose.model('Workout', WorkoutSchema);
 
